6232868409ca41b45d66e700725bcb552eacc5e9
jorgen eikemo <jorgen.eikemo@gmail.com>
2023-01-21 13:56:25 +0100

    conencted to api and updating

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-let countryName;
-let countryFlag;
+var countryIndex = {};
+const countryCount = 150;
 
 window.onload = async function () {
-    countryName = document.getElementById("country-name");
-    countryFlag = document.getElementById("country-flag");
-    GetData();
+    for (let i = 0; i < countryCount; i++) {
+        await GetData(i);
+        let country = document.createElement("div");
+        country.id = i;
+        country.innerText = (i + 1).toString() + ": " + countryIndex[i]["name"];
+        country.classList.add("country-list-name");
+        document.getElementById("country-list").append(country)
+        country.addEventListener("click", updateCountry);
+    }
 }
 
-async function GetData(){
-    let response = await fetch("https://restcountries.com/v3.1/all");
+async function GetData(num){
+    let  url = "https://restcountries.com/v3.1/all/";
+    let response = await fetch(url);
     let data = await response.json();
-    console.log(data);
-    // countryName.innerText = data[0]["name"]["official"];
-    // countryFlag.src = data[0]["flags"]["png"];
+    //console.log(data);
+    let countryName = data[num]["name"]["common"];
+    let officialName = data[num]["name"]["official"]
+    let countryFlag = data[num]["flags"]["png"];
+
+    countryIndex[num] = {"name" : countryName, "flag": countryFlag, "officialname": officialName};
+    //console.log(countryName + ", " + countryFlag);
+}
+
+function updateCountry(){
+    document.getElementById("country-name").innerText = countryIndex[this.id]["officialname"]
+    document.getElementById("country-flag").src = countryIndex[this.id]["flag"];
 }
