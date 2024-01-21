359c6786769f776f49bee94617cc2bfea7681571
Jorgen Eikemo <jorgen.eikemo@gmail.com>
2024-01-21 21:22:11 +0100

    refactoring code

--- script.js
+++ script.js
@@ -1,122 +1,128 @@
-// Initialization of variables
-let countryIndex = [{}];
-const countryCount = 250;
-let wikipediaLink;
-let loading = true;
-const debug = false;
+(function () {
+  const countryIndex = [];
 
-window.onload = async function () {
-  let statusText = document.getElementById("title");
-  let contentBox = document.getElementById("content-box");
-  updateCountry(); // Update displayed country information
-  await GetData(); // Asynchronously get data for each country
-  // Once data loading is complete, sort and create the list
-  sort();
-  statusText.innerHTML = "Countries of the World";
-  contentBox.classList.remove("d-none");
-  loading = false;
-  console.log(countryIndex);
-};
+  window.onload = async () => {
+    const statusText = document.getElementById("title");
+    const contentBox = document.getElementById("content-box");
+    const loadingText = document.getElementById("loading-text");
+    statusText.innerHTML = "Countries of the World";
+    await GetData();
+    loadingText.classList.add("d-none");
+    contentBox.classList.remove("d-none");
+    sort();
+    showRandomCountry();
+  };
 
-// Get data for all countries from an API
-async function GetData() {
-  let url = "https://restcountries.com/v3.1/all/";
-  let response = await fetch(url);
-  let data = await response.json();
+  function showRandomCountry() {
+    const randomCountry = Math.floor(Math.random() * countryIndex.length);
+    updateCountry(randomCountry);
+  }
 
-  // Iterate over each country data and populate the countryIndex
-  data.forEach((country, index) => {
-    let countryName = country["name"]["common"];
-    let officialName = country["name"]["official"];
-    let countryFlag = country["flags"]["svg"];
-    let capitol = country["capital"];
-    let continent = country["region"];
-    let population = country["population"].toLocaleString("en-GB");
+  async function GetData() {
+    const url = "https://restcountries.com/v3.1/all/";
+    //const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+    try {
+      //await delay(2000);
+      const response = await fetch(url);
+      const data = await response.json();
 
-    countryIndex[index] = {
-      name: countryName,
-      flag: countryFlag,
-      officialname: officialName,
-      region: continent,
-      capitol: capitol,
-      population: population,
+      data.forEach((country, index) => {
+        countryIndex[index] = {
+          name: country["name"]["common"],
+          flag: country["flags"]["svg"],
+          officialname: country["name"]["official"],
+          region: country["region"],
+          capitol: country["capital"],
+          population: country["population"].toLocaleString("en-GB"),
+        };
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  function updateCountry(id) {
+    const mappings = {
+      "country-name": "officialname",
+      "country-flag": "flag",
+      capitol: "capitol",
+      region: "region",
+      population: "population",
     };
-  });
-}
 
-function updateCountry() {
-  if (loading) {
-    //Hardcoded default values while page is loading.
-    document.getElementById("country-name").innerText = "Kingdom of Norway";
-    document.getElementById("country-flag").src = "https://flagcdn.com/no.svg";
-    document.getElementById("capitol").innerText = "Oslo";
-    document.getElementById("region").innerText = "Europe";
-    document.getElementById("population").innerText = "5,379,475";
-    wikipediaLink = "Norway";
-  } else {
-    document.getElementById("country-name").innerText =
-      countryIndex[this.id]["officialname"];
-    document.getElementById("country-flag").src = countryIndex[this.id]["flag"];
-    document.getElementById("capitol").innerText =
-      countryIndex[this.id]["capitol"];
-    document.getElementById("region").innerText =
-      countryIndex[this.id]["region"];
-    document.getElementById("population").innerText =
-      countryIndex[this.id]["population"];
-    wikipediaLink = countryIndex[this.id]["name"];
-  }
-}
+    for (const [elementId, property] of Object.entries(mappings)) {
+      const element = document.getElementById(elementId);
 
-function sort() {
-  countryIndex.sort(function (a, b) {
-    //Convert to lowercase for case insensitive sorting.
-    var x = a.name.toLowerCase();
-    var y = b.name.toLowerCase();
-    return x < y ? -1 : x > y ? 1 : 0;
-  });
-  createList();
-}
-
-// Function to create the list of countries in the HTML
-function createList() {
-  //Create a list item for each country retrieved from the API
-  for (i = 0; i < countryCount; i++) {
-    let country = document.createElement("li");
-    country.id = i;
-    country.innerText = countryIndex[i]["name"];
-    country.classList.add("country-list-name");
-    country.classList.add("list-group-item");
-    country.classList.add("border-bottom");
-    country.classList.add("fs-5");
-    country.classList.add("py-1");
-    document.getElementById("country-list").append(country);
-    country.addEventListener("click", updateCountry);
-  }
-  searchList();
-}
-
-// Function to enable searching the list of countries
-function searchList() {
-  const searchBox = document.getElementById("searchinput");
-  const list = document.getElementById("country-list");
-
-  //Add an eventlistener to the input field to check for changes
-  searchBox.addEventListener("input", function () {
-    const searchValue = this.value.toLowerCase();
-    const listItems = list.getElementsByTagName("li");
-    //Loop through contrylist to see if searchvalue present. Hide the ones that dont match searchvalue
-    for (let i = 0; i < listItems.length; i++) {
-      const listItem = listItems[i];
-      if (listItem.textContent.toLowerCase().includes(searchValue)) {
-        listItem.style.display = "block";
+      if (elementId === "country-flag") {
+        element.src = countryIndex[id][property];
       } else {
-        listItem.style.display = "none";
+        element.innerText = countryIndex[id][property];
       }
     }
-  });
-}
 
-// Function to open the Wikipedia page for a country in a new tab
-function wikipedia() {
+    const btn = document.getElementById("wikipedia-btn");
+    btn.setAttribute("data-wikipedia", countryIndex[id]["name"]);
+
+    updateList(id);
+  }
+
+  function updateList(id) {
+    const listItems = getListItems();
+    Array.from(listItems).forEach((l) => l.classList.remove("fw-bold"));
+
+    const listItem = document.getElementById(id);
+    listItem.classList.add("fw-bold");
+  }
+
+  function sort() {
+    countryIndex.sort((a, b) => {
+      const x = a.name.toLowerCase();
+      const y = b.name.toLowerCase();
+      return x < y ? -1 : 1;
+    });
+    createList();
+  }
+
+  function createList() {
+    countryIndex.forEach((c, i) => {
+      const country = document.createElement("li");
+      country.id = i;
+      country.innerText = countryIndex[i]["name"];
+      country.classList.add(
+        "country-list-name",
+        "list-group-item",
+        "border-bottom",
+        "fs-5",
+        "py-1"
+      );
+      document.getElementById("country-list").append(country);
+      country.addEventListener("click", () => updateCountry(country.id));
+    });
+    searchList();
+  }
+
+  function searchList() {
+    const searchBox = document.getElementById("searchinput");
+    const listItems = getListItems();
+
+    searchBox.addEventListener("input", function () {
+      const searchValue = this.value.toLowerCase();
+      Array.from(listItems).forEach((l) => {
+        l.style.display = l.textContent.toLowerCase().includes(searchValue)
+          ? "block"
+          : "none";
+      });
+    });
+  }
+
+  function getListItems() {
+    const list = document.getElementById("country-list");
+    const listItems = list.getElementsByTagName("li");
+    return listItems;
+  }
+})();
+
+function wikipedia(btn) {
+  const wikipediaLink = btn.getAttribute("data-wikipedia");
   window.open("https://en.wikipedia.org/wiki/" + wikipediaLink, "_blank");
 }
